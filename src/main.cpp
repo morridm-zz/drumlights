4953cdf77418337eb38e9cbe6279bbb6142ed3dd
David <morridm@gmail.com>
2018-03-13 21:21:15 -0500

    refactor main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@
   This code assumes you are using an Adafruit Trinket Pro 5v not the regular Trinket 5v.
 */
 
-
 #include <Arduino.h>
 #include <DrumPatterns.h>
 
@@ -21,7 +20,6 @@ static const int PIEZO_ANALOG_INPUT_PIN = 0;    // The analog alias pin # to the
 static const int PIEZO_DIGITAL_INPUT_PIN = 14;  // The digital pin # for the piezo input
 static const int POT_PIN = A1;
 static const int BUTTON_PIN = 12;
-
 
 //static const struct DrumComponent MIDDLE_TOM      = { 3, 65, 0, 0, NEOPIXEL_STRIP_SIGNAL_PIN, PIEZO_ANALOG_INPUT_PIN, PIEZO_DIGITAL_INPUT_PIN, POT_PIN };
 //static const struct DrumComponent HI_TOM          = { 3, 50, 10, 0, NEOPIXEL_STRIP_SIGNAL_PIN, PIEZO_ANALOG_INPUT_PIN, PIEZO_DIGITAL_INPUT_PIN, POT_PIN };
@@ -32,25 +30,6 @@ static const struct DrumComponent theDrum = {3, 81, 10, 0, NEOPIXEL_STRIP_SIGNAL
 void animationComplete();
 
 DrumPatterns strip(theDrum, theDrum.numPixelsOnDrum, theDrum.NEOPIXEL_STRIP_SIGNAL_PIN, NEO_GRB + NEO_KHZ800, &animationComplete);
-
-void setup()
-{
-
-    strip.begin();
-    strip.setBrightness(255);
-
-    strip.Scanner(strip.Color(0, 0, 255), 45);
-    strip.ColorWipe(strip.Color(0, 0, 255), 30);
-
-    strip.ActivePattern = DRUMBEAT;
-
-    pinMode(theDrum.PIEZO_DIGITAL_INPUT_PIN, INPUT);
-    pinMode(BUTTON_PIN, INPUT_PULLUP);
-
-    strip.clearStrip();
-    strip.colorWipeAnimation(strip.Color(0, 30, 0), 10); //show green color wipe to indicate it is ready to go
-    strip.clearStrip();
-}
 
 bool wasButtonPressed()
 {
@@ -82,6 +61,25 @@ bool wasButtonPressed()
     oldState = newState;
 
     return bReturn;
+}
+
+void setup()
+{
+
+    strip.begin();
+    strip.setBrightness(255);
+
+    strip.Scanner(strip.Color(0, 0, 255), 45);
+    strip.ColorWipe(strip.Color(0, 0, 255), 30);
+
+    strip.ActivePattern = DRUMBEAT;
+
+    pinMode(theDrum.PIEZO_DIGITAL_INPUT_PIN, INPUT);
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+    strip.clearStrip();
+    strip.colorWipeAnimation(strip.Color(0, 30, 0), 10); //show green color wipe to indicate it is ready to go
+    strip.clearStrip();
 }
 
 void loop()
